6241d3f0604b21fbee0a94d2a838894dd089ad3e
excelsior01 <58037068+excelsior01@users.noreply.github.com>
2021-03-20 13:24:45 +0000

    Update Firebase.js

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -2,13 +2,7 @@ import firebase from "firebase";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCnbvN4yj7CwdyBFEMBbjB--ekviJELqhw",
-    authDomain: "ecommerce1-2c2f3.firebaseapp.com",
-    projectId: "ecommerce1-2c2f3",
-    storageBucket: "ecommerce1-2c2f3.appspot.com",
-    messagingSenderId: "218287483727",
-    appId: "1:218287483727:web:49d46183342dcbf8dff33a",
-    measurementId: "G-D1HL7GNXQZ"
+    yourfirebaseconfig
   };
 
 //initializing firebase app
